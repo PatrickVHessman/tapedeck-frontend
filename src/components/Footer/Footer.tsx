64f7d6987efcdfded80aab3a5f224805f71343a3
Patrick Hessman <patrickvhessman@gmail.com>
2024-11-13 12:06:19 -0500

    added git link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
-    return (<footer id="footer">Created by <Link to="https://www.patrickvhessman.com/" target="_blank">Patrick Hessman</Link> | <Link to="https://github.com/PatrickVHessman/tapedeck-api" target="_blank">View API source code</Link> | <Link to="" target="_blank">View frontend source code</Link></footer>);
+    return (<footer id="footer">Created by <Link to="https://www.patrickvhessman.com/" target="_blank">Patrick Hessman</Link> | <Link to="https://github.com/PatrickVHessman/tapedeck-api" target="_blank">View API source code</Link> | <Link to="https://github.com/PatrickVHessman/tapedeck-frontend" target="_blank">View frontend source code</Link></footer>);
 }
